98ae267dfea4d5624f782f6c71992d4290dcda0b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-12-04 21:20:16 +0100

    Refactored code to be cleaner

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,97 +1,82 @@
 import { uiActions } from './ui-slice';
 import { cartActions } from './cart-slice';
 
-export const fetchCartData = () => {
-    return async (dispatch) => {
-        dispatch(
-            uiActions.showNotification({
-                open: true,
-                message: "Fetching Data",
-                type: "warning"
-            })
-        )
+const DB_URL = 'https://cart-project-4ee01-default-rtdb.firebaseio.com/cartItems.json';
 
-        const fetchRequest = async () => {
-            try {
-                const res = await fetch('https://cart-project-4ee01-default-rtdb.firebaseio.com/cartItems.json')
-                const data = await res.json();
-                return data;
-            } 
-            catch (err) {
-                console.log(err)
-            }
-        }
-
-        try {
-            const dat = await fetchRequest();
-
-            dispatch(
-                cartActions.setCart(dat)
-            )
-
-            dispatch(
-                cartActions.setChanged(false)
-            )
-
-            console.log(dat, typeof(dat))
-            dispatch(
-                uiActions.showNotification({
-                    open: true,
-                    message: "Fetching Data Successful!",
-                    type: "success"
-                })
-            )
-        } catch(err) {
-            dispatch(
-                uiActions.showNotification({
-                    open: true,
-                    message: "Fetching Data Failed!",
-                    type: "error"
-                })
-            )
-        }
-
+async function fetchRequest () {
+    try {
+        const res = await fetch(DB_URL)
+        const data = await res.json();
+        return data;
     }
-};
-
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(
-            uiActions.showNotification({
-                open: true,
-                message: "Sending Request",
-                type: "warning"
-            })
-        )
-
-        const sendRequest = async () => {
-
-            const res = await fetch('https://cart-project-4ee01-default-rtdb.firebaseio.com/cartItems.json', {
-                method: 'PUT',
-                body: JSON.stringify(cart)
-            });
-            
-            await res.json();
-
-            dispatch(cartActions.setChanged(false))
-
-            dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sent request to DataBase successfully!",
-                type: 'success'
-            }))
-
-        };
-
-        try{
-            await sendRequest();
-        } catch(err) {
-            dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sending request failed.",
-                type: 'error'
-            }));
-        }
-
+    catch (err) {
+        console.error(err)
     }
 }
+
+async function sendRequest(cart, dispatch) {
+    try {
+        const options = { method: 'PUT', body: JSON.stringify(cart) };
+        const res = await fetch(DB_URL, options);
+        await res.json();
+        dispatch(cartActions.setChanged(false));
+        dispatch(uiActions.showNotification({
+            open: true,
+            message: "Sent request to DataBase successfully!",
+            type: 'success'
+        }))
+    } catch (error) {
+        console.error(error);
+    }
+
+};
+
+export const fetchCartData = () => async function (dispatch) {
+    dispatch(
+        uiActions.showNotification({
+            open: true,
+            message: "Fetching Data",
+            type: "warning"
+        })
+    );
+    try {
+        const dat = await fetchRequest();
+        dispatch(cartActions.setCart(dat));
+        dispatch(cartActions.setChanged(false));
+        dispatch(
+            uiActions.showNotification({
+                open: true,
+                message: "Fetching Data Successful!",
+                type: "success"
+            })
+        )
+    } catch (err) {
+        dispatch(
+            uiActions.showNotification({
+                open: true,
+                message: "Fetching Data Failed!",
+                type: "error"
+            })
+        )
+    }
+
+};
+
+export const sendCartData = (cart) => async function (dispatch) {
+    dispatch(
+        uiActions.showNotification({
+            open: true,
+            message: "Sending Request",
+            type: "warning"
+        })
+    );
+    try {
+        await sendRequest(cart, dispatch);
+    } catch (err) {
+        dispatch(uiActions.showNotification({
+            open: true,
+            message: "Sending request failed.",
+            type: 'error'
+        }));
+    }
+};
