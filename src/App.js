83f6e99b9632ff29608f69221bf87ac47a48112c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-12-04 21:22:03 +0100

    Removed unnecessary code comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Login from './components/Login';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-//import { uiActions } from './store/ui-slice';
 import { sendCartData, fetchCartData } from './store/cart-actions';
 import Products from './components/Products';
 import Notification from './components/Notification';
@@ -12,43 +11,12 @@ function App() {
   const notification = useSelector( state => state.ui.notification );
   const dispatch = useDispatch();
   const cart = useSelector( state => state.cart );
-  //const apiUrl = 'https://cart-project-4ee01-default-rtdb.firebaseio.com/cartItems.json';
   
   useEffect( () => {
 
     if(!cart.changed) return;
     dispatch(sendCartData(cart))
     dispatch(fetchCartData())
-
-    /*const sendRequest = async () => {
-
-      dispatch(uiActions.showNotification({
-        open: true,
-        message: "Sending...",
-        type: 'warning'
-      }))
-
-      const res = await fetch(apiUrl, {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      });
-
-      const data = await res.json();
-
-      dispatch(uiActions.showNotification({
-        open: true,
-        message: "Sent request to DataBase successfully!",
-        type: 'success'
-      }))
-    };
-
-    sendRequest().catch(err => {
-      dispatch(uiActions.showNotification({
-        open: true,
-        message: "Sending request failed.",
-        type: 'error'
-      }))
-    });*/
 
   }, [cart, dispatch])
 
